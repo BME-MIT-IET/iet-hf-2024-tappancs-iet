83176a5ea73e6b35593bf80c0749f45c93f9ce7b
bigaron <bigaron2@gmail.com>
2024-05-22 00:49:09 +0200

    Random isnt recreated each function call fix

--- drukmakori_sivatag/src/main/java/drukmakori_sivatag/Pipe.java
+++ drukmakori_sivatag/src/main/java/drukmakori_sivatag/Pipe.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Pipe extends Field{
     boolean Occupied = false;
-
+    private Random randomGen = new Random();
 
     boolean HasWater = false;
     boolean Slippery = false;
@@ -84,7 +84,7 @@ public class Pipe extends Field{
     @Override
     public void Fix(){
         IsBroken=false;
-        TicksUntilBreakable=new Random().nextInt(10);
+        TicksUntilBreakable=randomGen.nextInt(10);
     }
     /**
      * A függvény nem csinál semmit, mivel nem lehet csőre csövet kötni.
@@ -126,7 +126,7 @@ public class Pipe extends Field{
                 p.MoveTo(Neighbors.get(0));
             } else {
                 if(Main.isRandomEnabled) {
-                    p.MoveTo(Neighbors.get(new Random().nextInt(0,2)));
+                    p.MoveTo(Neighbors.get(randomGen.nextInt(0,2)));
                 } else {
                     p.MoveTo(Neighbors.get(0));
                 }
