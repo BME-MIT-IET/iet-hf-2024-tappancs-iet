b2087412d3e6743f925a8754d4aa82326085cd9c
bigaron <bigaron2@gmail.com>
2024-05-22 21:09:30 +0200

    reduced code complexity in Place

--- drukmakori_sivatag/src/main/java/drukmakori_sivatag/Functions.java
+++ drukmakori_sivatag/src/main/java/drukmakori_sivatag/Functions.java
@@ -172,14 +172,7 @@ public class Functions {
      */
     public static void Place(String[] args){
         boolean success = false;
-        if(args.length < 2){
-            Main.WriteIntoFilesAndConsole("Not enough arguments for Place",false);
-            return;
-        }
-        else if(args.length > 2){
-            Main.WriteIntoFilesAndConsole("Too many arguments for Place",false);
-            return;
-        }
+        if(!argCheck(args, 2, "Not enough arguments for Place", "Too many arguments for Place")) return;
 
         Mechanic player = Main.GetAsMechanic(Main.selectedPlayer);
         String playerName = Main.selectedPlayer;
